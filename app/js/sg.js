0e87194773d547ca9d7b6137ec3450e4f1ee0185
Scott Gingras <greanbaby@gmail.com>
2023-05-21 08:38:26 -0600

    Overlay fading out properly using visibility and opacity

--- app/js/sg.js
+++ app/js/sg.js
@@ -5,10 +5,25 @@
  * 
  * @Author: Scott Gingras May 2023
  */
-const btnHamburg = document.querySelector('#btnHamburger');
-const header = document.querySelector('.header');
+const btnHamburg = document.querySelector('#btnHamburger'),
+  header = document.querySelector('.header'),
+  overlay = document.querySelector('.overlay');
 
 btnHamburg.addEventListener('click', function() {
   console.log('Ham Burger Clicked');
-  header.classList.toggle('open');
+  
+  // Check if header contains 'open' class and if so
+  // remove the class from the OVERLAY and the HEADER
+  if (header.classList.contains('open')) {
+    header.classList.remove('open');
+    overlay.classList.add('fade-out');
+    overlay.classList.remove('fade-in');
+
+  } else {
+    header.classList.add('open');
+    overlay.classList.add('fade-in');
+    overlay.classList.remove('fade-out');
+
+  }
+
 });
