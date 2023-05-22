48b4b7d69be36ccea378ec595b2c7e46c34efb56
Scott Gingras <greanbaby@gmail.com>
2023-05-21 20:43:57 -0600

    Mobile menu animated fading in/out

--- app/js/sg.js
+++ app/js/sg.js
@@ -7,20 +7,30 @@
  */
 const btnHamburg = document.querySelector('#btnHamburger'),
   header = document.querySelector('.header'),
-  overlay = document.querySelector('.overlay');
+  overlay = document.querySelector('.overlay'),
+  fadeElems = document.querySelectorAll('.has-fade');
 
 btnHamburg.addEventListener('click', function() {
+
   console.log('Ham Burger Clicked');
   
   // Check if header contains 'open' class and if so
   // remove the class from the OVERLAY and the HEADER
   if (header.classList.contains('open')) {
     header.classList.remove('open');
+    fadeElems.forEach(function(element) {
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    });
     overlay.classList.add('fade-out');
     overlay.classList.remove('fade-in');
 
   } else {
     header.classList.add('open');
+    fadeElems.forEach(function(element) {
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+    });
     overlay.classList.add('fade-in');
     overlay.classList.remove('fade-out');
 
